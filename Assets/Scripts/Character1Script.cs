1400f09cd3e79764f07b30afcccde732356a714a
unknown <sbh9278@IGMResearch-07.main.ad.rit.edu>
2015-11-08 17:02:37 -0500

    Added in player movemet, player impact resolution,
    and set up a test scene to showcase these.

---
+++ Assets/Scripts/Character1Script.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character1Script : MonoBehaviour {
+
+	//The acceleration for movement
+	private Vector3 acceleration;
+	//The velocity for movement
+	public Vector3 velocity;
+
+	// Update is called once per frame
+	void Update () {
+		//Getting player input and incrementing acceleration
+		if(Input.GetKey(KeyCode.A)){
+			acceleration +=(transform.right*-.001f);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			acceleration +=(transform.right*.001f);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			acceleration +=(transform.forward*-.001f);
+		}
+		if(Input.GetKey(KeyCode.W)){
+			acceleration +=(transform.forward*.001f);
+		}
+
+		//Clamping the velocity so players can't go too fast
+		Vector3.ClampMagnitude (velocity, 1);
+
+		//Adding the acceleration from this update to the overall velocity.
+		velocity += acceleration;
+
+		//Zeroing the velocity for the next update
+		acceleration = Vector3.zero;
+
+		//Adding the velocity to the object's overall position
+		transform.position+=velocity;
+	}
+	
+	void OnCollisionEnter(Collision col){
+		//If players are colliding then we mimic a somewhat inelastic collision by swapping their velocities and setting them to 80%
+		if (col.gameObject.tag == "Player") {
+			Vector3 otherVelo = col.gameObject.GetComponent<Character2Script> ().velocity;
+			col.gameObject.GetComponent<Character2Script> ().velocity = (velocity*.8f);
+			velocity= (otherVelo*.8f);
+		}
+	}
+}
---
+++ Assets/Scripts/Character2Script.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character2Script : MonoBehaviour {
+
+	private Vector3 acceleration;
+	public Vector3 velocity;
+	private Rigidbody contr;
+	
+	
+	// Use this for initialization
+	void Start () {
+		contr = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Getting player input
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			acceleration +=(transform.right*-.001f);
+		}
+		if(Input.GetKey(KeyCode.RightArrow)){
+			acceleration +=(transform.right*.001f);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)){
+			acceleration +=(transform.forward*-.001f);
+		}
+		if(Input.GetKey(KeyCode.UpArrow)){
+			acceleration +=(transform.forward*.001f);
+		}
+		
+		//Clamping the velocity so players can't go too fast
+		Vector3.ClampMagnitude (velocity, 1);
+		
+		//Adding the acceleration from this update to the overall velocity.
+		velocity += acceleration;
+		
+		//Zeroing the velocity for the next update
+		acceleration = Vector3.zero;
+		
+		//Adding the velocity to the object's overall position
+		transform.position+=velocity;
+	}
+}
