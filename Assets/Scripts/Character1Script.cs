69729a6358d60d3043f8b946d2b47a87a90f2fa6
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-11-13 12:01:38 -0500

    Add base prefab for gameplay UI

--- Assets/Scripts/Character1Script.cs
+++ Assets/Scripts/Character1Script.cs
@@ -2,17 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Character1Script : MonoBehaviour {
+public class Character1Script : MonoBehaviour
+{
 
-	//This number represents the number of the player to be used for control options
-	public int PlayerNumber;
-	//The acceleration for movement
-	private Vector3 acceleration;
-	//The velocity for movement
-	public Vector3 velocity;
-	//This boolean checks if the velocity has already been swapped as a result of a collision,
-	//and keeps us from swapping velocities twice in a collision
-	private bool velocitySwapped= false;
+    //This number represents the number of the player to be used for control options
+    public int PlayerNumber;
+    //The acceleration for movement
+    private Vector3 acceleration;
+    //The velocity for movement
+    public Vector3 velocity;
+    //This boolean checks if the velocity has already been swapped as a result of a collision,
+    //and keeps us from swapping velocities twice in a collision
+    private bool velocitySwapped = false;
 
     private float health;
     private float speed = 1.0f;
@@ -35,106 +36,134 @@ public class Character1Script : MonoBehaviour {
         yield return new WaitForSeconds(0.5f);
     }
 
-	// Update is called once per frame
-	void Update () {
-		if (PlayerNumber == 0) {
-			//Getting player input and incrementing acceleration
-			if (Input.GetKey (KeyCode.A)) {
-				acceleration += (transform.right * -.001f);
-			}
-			if (Input.GetKey (KeyCode.D)) {
-				acceleration += (transform.right * .001f);
-			}
-			if (Input.GetKey (KeyCode.S)) {
-				acceleration += (transform.forward * -.001f);
-			}
-			if (Input.GetKey (KeyCode.W)) {
-				acceleration += (transform.forward * .001f);
-			}
-		}
-		if (PlayerNumber == 1) {
-			//Getting player input
-			if(Input.GetKey(KeyCode.LeftArrow)){
-				acceleration +=(transform.right*-.001f);
-			}
-			if(Input.GetKey(KeyCode.RightArrow)){
-				acceleration +=(transform.right*.001f);
-			}
-			if(Input.GetKey(KeyCode.DownArrow)){
-				acceleration +=(transform.forward*-.001f);
-			}
-			if(Input.GetKey(KeyCode.UpArrow)){
-				acceleration +=(transform.forward*.001f);
-			}
-		}
-		if (PlayerNumber == 2) {
-			//Getting player input
-			if(Input.GetKey(KeyCode.F)){
-				acceleration +=(transform.right*-.001f);
-			}
-			if(Input.GetKey(KeyCode.H)){
-				acceleration +=(transform.right*.001f);
-			}
-			if(Input.GetKey(KeyCode.G)){
-				acceleration +=(transform.forward*-.001f);
-			}
-			if(Input.GetKey(KeyCode.T)){
-				acceleration +=(transform.forward*.001f);
-			}
-		}
-		if (PlayerNumber == 3) {
-			//Getting player input
-			if(Input.GetKey(KeyCode.J)){
-				acceleration +=(transform.right*-.001f);
-			}
-			if(Input.GetKey(KeyCode.L)){
-				acceleration +=(transform.right*.001f);
-			}
-			if(Input.GetKey(KeyCode.K)){
-				acceleration +=(transform.forward*-.001f);
-			}
-			if(Input.GetKey(KeyCode.I)){
-				acceleration +=(transform.forward*.001f);
-			}
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale == 0.0f) return;
 
-		/*if (PlayerNumber == 0) {
-			Debug.Log ("Player 0 Velo: " + velocity.magnitude);
-		}
-		if (PlayerNumber == 1) {
-			Debug.Log ("Player 1 Velo: " + velocity.magnitude);
-		}*/
+        if (PlayerNumber == 0)
+        {
+            //Getting player input and incrementing acceleration
+            if (Input.GetKey(KeyCode.A))
+            {
+                acceleration += (transform.right * -.001f);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                acceleration += (transform.right * .001f);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                acceleration += (transform.forward * -.001f);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                acceleration += (transform.forward * .001f);
+            }
+        }
+        if (PlayerNumber == 1)
+        {
+            //Getting player input
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                acceleration += (transform.right * -.001f);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                acceleration += (transform.right * .001f);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                acceleration += (transform.forward * -.001f);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                acceleration += (transform.forward * .001f);
+            }
+        }
+        if (PlayerNumber == 2)
+        {
+            //Getting player input
+            if (Input.GetKey(KeyCode.F))
+            {
+                acceleration += (transform.right * -.001f);
+            }
+            if (Input.GetKey(KeyCode.H))
+            {
+                acceleration += (transform.right * .001f);
+            }
+            if (Input.GetKey(KeyCode.G))
+            {
+                acceleration += (transform.forward * -.001f);
+            }
+            if (Input.GetKey(KeyCode.T))
+            {
+                acceleration += (transform.forward * .001f);
+            }
+        }
+        if (PlayerNumber == 3)
+        {
+            //Getting player input
+            if (Input.GetKey(KeyCode.J))
+            {
+                acceleration += (transform.right * -.001f);
+            }
+            if (Input.GetKey(KeyCode.L))
+            {
+                acceleration += (transform.right * .001f);
+            }
+            if (Input.GetKey(KeyCode.K))
+            {
+                acceleration += (transform.forward * -.001f);
+            }
+            if (Input.GetKey(KeyCode.I))
+            {
+                acceleration += (transform.forward * .001f);
+            }
+        }
 
-		//Clamping the velocity so players can't go too fast
-		Vector3.ClampMagnitude (velocity, 1);
+        /*if (PlayerNumber == 0) {
+            Debug.Log ("Player 0 Velo: " + velocity.magnitude);
+        }
+        if (PlayerNumber == 1) {
+            Debug.Log ("Player 1 Velo: " + velocity.magnitude);
+        }*/
 
-		//Adding the acceleration from this update to the overall velocity.
-		velocity += acceleration * speed;
+        //Clamping the velocity so players can't go too fast
+        Vector3.ClampMagnitude(velocity, 1);
 
-		//Zeroing the velocity for the next update
-		acceleration = Vector3.zero;
+        //Adding the acceleration from this update to the overall velocity.
+        velocity += acceleration * speed;
 
-		//Adding the velocity to the object's overall position
-		transform.position+=velocity;
-	}
+        //Zeroing the velocity for the next update
+        acceleration = Vector3.zero;
 
-	void ResolveBounce(Vector3 newVelo){
-		velocity = newVelo;
-		velocitySwapped = true;
-	}
-	
-	void OnCollisionEnter(Collision col){
-		if (!velocitySwapped) {
-			//If players are colliding then we mimic a somewhat inelastic collision by swapping their velocities and setting them to 80%
-			if (col.gameObject.tag == "Player") {
-				Vector3 tempVec= col.gameObject.GetComponent<Character1Script> ().velocity;
-				col.gameObject.GetComponent<Character1Script> ().ResolveBounce(((col.transform.position - transform.position).normalized) * velocity.magnitude * .8f);
-				ResolveBounce(((transform.position- col.transform.position).normalized) * tempVec.magnitude * .8f);
-			}
-		}
-	}
+        //Adding the velocity to the object's overall position
+        transform.position += velocity;
+    }
 
-	void OnCollisionExit(){
-		velocitySwapped = false;
-	}
+    void ResolveBounce(Vector3 newVelo)
+    {
+        velocity = newVelo;
+        velocitySwapped = true;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (!velocitySwapped)
+        {
+            //If players are colliding then we mimic a somewhat inelastic collision by swapping their velocities and setting them to 80%
+            if (col.gameObject.tag == "Player")
+            {
+                Vector3 tempVec = col.gameObject.GetComponent<Character1Script>().velocity;
+                col.gameObject.GetComponent<Character1Script>().ResolveBounce(((col.transform.position - transform.position).normalized) * velocity.magnitude * .8f);
+                ResolveBounce(((transform.position - col.transform.position).normalized) * tempVec.magnitude * .8f);
+            }
+        }
+    }
+
+    void OnCollisionExit()
+    {
+        velocitySwapped = false;
+    }
 }
---
+++ Assets/Scripts/GameplayEvents.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplayEvents : MonoBehaviour
+{
+    private bool gamePaused = false;
+    private GameObject pausePanel;
+
+    public void Start()
+    {
+        pausePanel = GameObject.FindGameObjectWithTag("pausePanel");
+        pausePanel.SetActive(false);
+    }
+
+    public void TogglePauseState()
+    {
+        if (gamePaused)
+        {
+            Time.timeScale = 1.0f;
+            gamePaused = false;
+            pausePanel.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0.0f;
+            gamePaused = true;
+            pausePanel.SetActive(true);
+        }
+    }
+
+    public void ResetLevel()
+    {
+        TogglePauseState();
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
