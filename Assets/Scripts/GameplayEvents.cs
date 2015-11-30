e571fe2d2109b2d825d65a1934d7068004364a7e
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-11-30 17:31:47 -0500

    Add splash screen

--- Assets/Scripts/GameplayEvents.cs
+++ Assets/Scripts/GameplayEvents.cs
@@ -4,12 +4,34 @@ using System.Collections;
 public class GameplayEvents : MonoBehaviour
 {
     private bool gamePaused = false;
+    private GameObject pauseButton;
     private GameObject pausePanel;
+    private GameObject splashScreen;
 
     public void Start()
     {
+        pauseButton = GameObject.FindGameObjectWithTag("pauseButton");
         pausePanel = GameObject.FindGameObjectWithTag("pausePanel");
+        splashScreen = GameObject.FindGameObjectWithTag("splashScreen");
+
+        // Activate Splash Screen
+        splashScreen.SetActive(true);
+        pauseButton.SetActive(false);
         pausePanel.SetActive(false);
+        Time.timeScale = 0.0f;
+        gamePaused = true;
+        StartCoroutine(EndSplashScreen());
+    }
+
+
+    public IEnumerator EndSplashScreen()
+    {
+        Debug.Log("wait");
+        yield return new WaitForSeconds(1);
+        Debug.Log("waited");
+        pauseButton.SetActive(true);
+        pausePanel.SetActive(true);
+        splashScreen.SetActive(false);
     }
 
     public void TogglePauseState()
@@ -19,12 +41,14 @@ public class GameplayEvents : MonoBehaviour
             Time.timeScale = 1.0f;
             gamePaused = false;
             pausePanel.SetActive(false);
+            pauseButton.SetActive(true);
         }
         else
         {
             Time.timeScale = 0.0f;
             gamePaused = true;
             pausePanel.SetActive(true);
+            pauseButton.SetActive(false);
         }
     }
 
